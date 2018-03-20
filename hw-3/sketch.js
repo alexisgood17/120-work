966cdf75a4c3bc164c3aabe33ef79cc012c10118
Alexis Good <algood1799@gmail.com>
2018-03-20 14:11:05 -0600

    created p5 sketch for hw part one

---
+++ hw-3/sketch.js
@@ -0,0 +1,14 @@
+// declaring the setup function
+function setup() {
+  // creating a canvas 600px wide and 400px high
+  createCanvas(600, 400);
+  // coloring the bg 'pink'
+  background('pink');
+}
+
+// declaring draw function
+function draw() {
+  // telling p5 to print the string "Hello World!"
+  // positioned at (x: 20, y: 30)
+  text( "Hello World!", 20, 30 );
+}
