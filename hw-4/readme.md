6f99022fafa673b0212e7776a1587dcf0869efb3
Alexis Good <algood1799@gmail.com>
2018-04-30 19:49:29 -0600

    finished hw-4

--- hw-4/readme.md
+++ hw-4/readme.md
@@ -1,3 +1,7 @@
-# Empty p5 sketch.
-
-Please replace contents of this file with appropriate readme information after finishing your p5 sketch.
+# Alexis Good
+  ## 1. Work Done This Homework Cycle
+  This week, I had to create a self portrait using p5. I was able to look at a couple of examples in order to figure out the best way to create my image. I ended up making a (sort of) self portrait.
+  ## 2. Steps Taken
+  First, I had to come up with an idea for what I wanted to create. Then, I figured out how to break down the picture into individual shapes. I separated each component into its own block of code and broke down those parts even further to completely finish my portrait.
+ ## 3. Issues and Questions
+  There was a lot of trial and error involved in creating my portrait. It was difficult to visualize the grid in my head, so I ended up changing the coordinates for shapes many times. It was time consuming work to tweak every little part of the image and then check back with the index.html file every time I needed to change something. In the end, I am fairly satisfied with the way my portrait turned out.
---
+++ hw-4/sketch.js
@@ -0,0 +1,180 @@
+function setup() {
+    // create a canvas to draw on
+    createCanvas( 800, 800 );
+}
+
+function draw() {
+
+    // background color
+    background( 'rgb(37, 80, 70)' );
+
+    /* ***************************** */
+    /* MAIN SANDBOX */
+    /* ***************************** */
+    push();
+
+    // set the grid center( x:0, y:0 )
+    // to canvas center
+    translate( 400, 400 );
+
+
+    /* ***************************** */
+    /* ARMS */
+    /* ***************************** */
+    push();
+    translate( 0, -55 );
+
+    // right arm
+    rotate( PI );
+    stroke( 'rgba(59, 11, 11, 1)' );
+    strokeWeight( 50 );
+    line( -120, -25, -150, -100 );
+    line( -150, -100, -200, -200 );
+
+    // hand
+    stroke( 'rgb(242, 183, 154)' );
+    strokeWeight( 75 );
+    point( -200, -200 );
+
+    // left arm
+    scale( -1, 1 );
+    stroke( 'rgba(59, 11, 11, 1)' );
+    strokeWeight( 50 );
+    line( -120, -25, -150, -100 );
+    line( -150, -100, -200, -200 );
+
+    // hand
+    stroke( 'rgb(242, 183, 154)' );
+    strokeWeight( 75 );
+    point( -200, -200 );
+
+    pop(); // ARMS END
+
+
+    /* ***************************** */
+    /* BODY */
+    /* ***************************** */
+    push();
+    // dress triangles
+    fill( 'rgba(59, 11, 11, 1)' );
+    noStroke();
+    triangle( -125, -50, 125, -50, 0, 200);
+    triangle( -125, 250, 125, 250, 0, -50);
+    pop();
+    push();
+    //add shirt collar
+    fill( 'rgb(rgb(255, 255, 255))')
+    noStroke();
+    triangle( -50, -50, 0, -50, -50, 0 );
+    //flip x-axis with scale
+    scale( -1, 1 );
+    triangle( -50, -50, 0, -50, -50, 0 );
+
+
+    pop(); // BODY END
+
+
+    /* ***************************** */
+    /* LEGS */
+    /* ***************************** */
+    push();
+    translate( 0, 200 );
+
+    // left leg
+    fill( 'rgba(240, 213, 167, 1)' );
+    noStroke();
+    quad( -100, 50, -5, 50, -150, 300, -100, 300);
+    // scale to flip the shape
+    scale( -1, 1 );
+    quad( -100, 50, -5, 50, -150, 300, -100, 300 );
+    pop();
+    // LEGS END
+
+    //shoes
+    push();
+    fill( 'rgb( 0, 0, 0 )')
+    triangle( -70, 400, -70, 370, -150, 400);
+    scale( -1, 1);
+    triangle( -70, 400, -70, 370, -150, 400);
+    pop();
+
+    /* ***************************** */
+    /* HEAD */
+    /* ***************************** */
+
+    //hair
+    push();
+    fill('rgb(43, 33, 23)');
+    noStroke();
+    arc(0, -75, 500, 500, PI, 0, CHORD);
+    pop();
+
+    //face 
+    push();
+    translate( 0, -175 );
+
+    fill( 'rgba(240, 213, 167, 1)' );
+    noStroke();
+    ellipse(0, 0, 250);
+
+    // mouth
+    push();
+    stroke('rgb(144, 25, 25)');
+    noFill();
+    curve(0, 0, -50, 50, 50, 50, 0, 0 );
+    pop();
+
+    // nose
+    push();
+    fill( 'rgb(242, 183, 154)' );
+    triangle( 0, -20, 10, 20, -10, 20 );
+    pop();
+    push();
+    fill( 'rgb(209, 136, 109)' );
+    triangle( 0, 10, 10, 20, -10, 20 );
+    pop();
+
+    //glasses
+    push();
+    noFill();
+    stroke( 'rgb(0, 0, 0)');
+    strokeWeight(4);
+    rect(20, -40, 75, 50);
+    scale(-1, 1);
+    rect(20, -40, 75, 50);
+    line(-20, -10, 20, -10);
+    pop();
+
+
+    // eyes
+    // left
+    push();
+    translate( -60, -40 );
+
+    noFill();
+    fill( 'rgb(45, 28, 21)' );
+    ellipse( 5, 25, 30 );
+    fill( 0 );
+    ellipse( 5, 25, 20 );
+
+    pop();
+
+    // right
+    push();
+    translate( 60, -40 );
+
+    noFill();
+    fill( 'rgb(45, 28, 21)' );
+    ellipse( -5, 25, 30 );
+    fill( 0 );
+    ellipse( -5, 25, 20 );
+
+    pop();
+
+    pop(); // HEAD END
+
+
+
+
+    pop();
+}
